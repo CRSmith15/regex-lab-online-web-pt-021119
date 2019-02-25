113326401837717ae344047cb0122f6e37b00081
Chance Smith <chancesmith97@hotmail.com>
2019-02-25 02:22:33 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,12 +1,19 @@
 def starts_with_a_vowel?(word)
+  if /^[^aeiou\W]/i.match(word) == nil
+    return true
+  else
+    return false
+  end
 
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
+  text.scan(/\b[Uu]n[a-z]*ing\b/)
 
 end
 
 def words_five_letters_long(text)
+  text.scan(/\w{5}$/)
 
 end
 
